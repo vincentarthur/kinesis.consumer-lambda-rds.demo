416a80c9f6ee2d3236a7649947a586e3518db847
Vincent W M CHEN <443519782@qq.com>
2017-09-17 15:12:39 +0800

    initial commit

---
+++ src/main/java/kinesis/lambda/rds/demo/Write2Rds.java
@@ -0,0 +1,92 @@
+package kinesis.lambda.rds.demo;
+
+import com.amazonaws.services.lambda.runtime.events.KinesisEvent;
+import kinesis.lambda.rds.demo.entity.SampleRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Created by vwmc on 9/17/17.
+ */
+public class Write2Rds {
+
+    private Logger logger = LoggerFactory.getLogger(Write2Rds.class);
+
+    private final String INSTANCE_NAME = "vcrdsinstance";
+    private final String DB_NAME = "VC_RDS_DEMO";
+    private final String USER_NAME = "vc_rds_root";
+    private final String PASSWORD = "vc_rds_root";
+
+    private final String TABLE_NAME = "RDS_DEMO";
+
+    private final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final String END_POINT = "jdbc:mysql://vcrdsinstance.c0folbvxtjiy.ap-southeast-2.rds.amazonaws.com:3306/" + DB_NAME;
+
+
+    public void receiveRecords(KinesisEvent event) {
+
+        event.getRecords().forEach(
+                record ->
+                        write2RDDS(SampleRecord.fromJsonAsBytes(record.getKinesis().getData().array()))
+        );
+
+    }
+
+    /**
+     * @param sampleRecord
+     */
+    private void write2RDDS(SampleRecord sampleRecord) {
+
+        try {
+            Class.forName(MYSQL_DRIVER);
+        } catch (ClassNotFoundException e) {
+            logger.error(e.getLocalizedMessage());
+        }
+
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(END_POINT, USER_NAME, PASSWORD);
+        } catch (SQLException e) {
+            logger.error(e.getSQLState());
+        }
+
+
+        PreparedStatement statement = null;
+
+        try {
+            statement = connection.prepareStatement("INSERT INTO " + TABLE_NAME + " values (?,?)");
+            statement.setInt(1, Integer.parseInt(sampleRecord.getId()));
+            statement.setString(2, sampleRecord.getContent());
+
+            int resultCnt = statement.executeUpdate();
+
+            logger.info("Inserted  : " + resultCnt + " record with ID[" + sampleRecord.getId() + "].");
+
+        } catch (SQLException e) {
+            logger.error(e.getSQLState());
+        }finally{
+
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            if(connection!=null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ src/test/java/kinesis/lambda/rds/demo/LambdaRDSDemo.java
@@ -0,0 +1,65 @@
+package kinesis.lambda.rds.demo;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+/**
+ * Created by vwmc on 9/17/17.
+ */
+public class LambdaRDSDemo {
+
+    private final String INSTANCE_NAME = "vcrdsinstance";
+    private final String DB_NAME = "VC_RDS_DEMO";
+    private final String USER_NAME = "vc_rds_root";
+    private final String PASSWORD = "vc_rds_root";
+
+    private final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final String END_POINT = "jdbc:mysql://vcrdsinstance.c0folbvxtjiy.ap-southeast-2.rds.amazonaws.com:3306/" + DB_NAME;
+
+    private Logger logger = LoggerFactory.getLogger(LambdaRDSDemo.class);
+
+    private boolean TABLE_CREATED = true;
+
+//    @Test
+    public void testConnection() throws ClassNotFoundException, SQLException {
+
+        logger.info("To Test connection");
+
+        Class.forName(MYSQL_DRIVER);
+        Connection connection = DriverManager.getConnection(END_POINT, USER_NAME, PASSWORD);
+
+        String sql = null;
+
+        if (!TABLE_CREATED) {
+            sql = "CREATE TABLE RDS_DEMO(ID INT, NAME VARCHAR(10))";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            int result = statement.executeUpdate();
+            logger.info("executed with result " + result);
+            statement.close();
+
+        } else {
+
+            sql = "INSERT INTO RDS_DEMO VALUES (?,?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, (int) (Math.random() * 1000));
+            statement.setString(2, "test");
+            int effectedCnt = statement.executeUpdate();
+            logger.info("How many updated : " + effectedCnt);
+        }
+
+        connection.close();
+
+    }
+
+//    private AmazonRDS initAmazonRDSClientBuilder() throws Exception {
+//        return AmazonRDSClientBuilder.standard()
+//                .withCredentials(CredentialUtils.getCredentialsProvider())
+//                .withClientConfiguration(ConfigurationUtils.getClientConfigWithUserAgent())
+//                .build();
+//    }
+
+}
